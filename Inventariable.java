d74181cff1dc3025ac5a4c6adbb5a54ddc47e468
jandrogarrido791 <9212355939aledelacruz@gmail.com>
2026-03-22 18:47:18 -0600

    Update and rename Inventariable.java to Inventariable.java

--- Inventariable.java
+++ Inventariable.java
@@ -1,1 +1,16 @@
-Inventariable.java
+package modelo;
+
+/**
+ * Interfaz Inventariable
+ * Define el comportamiento que deben tener todos los productos en el inventario.
+ */
+public interface Inventariable {
+
+    void agregarAlInventario(int cantidad);
+
+    void retirarDelInventario(int cantidad);
+
+    boolean estaDisponible();
+
+    String generarReporte();
+}
